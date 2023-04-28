ee1b14c877c552f498194628c01bbfa5a2afa9c0
Pravesh Singh <praveshkumarsingh701@gmail.com>
2023-04-28 15:29:02 +0530

    Added Roboto Font Family.

---
+++ src/constants/color.tsx
@@ -0,0 +1,10 @@
+export default {
+    primary: "#0373F3",
+    modalBg: "rgba(255, 255, 255, 0.9)",
+    mediumBlack: "rgba(0, 0, 0, 0.86)",
+    lightMediumBlack: "rgba(0, 0, 0, 0.36)",
+    silver: "#C4C4C4",
+    white: "#ffffff",
+    errorRed: "#C41A16",
+    lightBlue: "#E4ECF5",
+};
